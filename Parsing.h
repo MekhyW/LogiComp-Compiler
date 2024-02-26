e57334a559f5cc276d816725b6d024b58b4fce1e
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-02-25 22:33:06 -0300

    Update Parsing.h

--- Parsing.h
+++ Parsing.h
@@ -67,23 +67,19 @@ private:
 public:
     static int parse_expression() {
         int result = 0;
-        Token current_token = tokenizer.selectNext();
         string op = "PLUS";
+        Token current_token = tokenizer.selectNext();
+        if (current_token.type == "EOF") { throw invalid_argument("Input cannot be empty"); }
         while (current_token.type != "EOF") {
             if (current_token.type == "INT") {
-                if (op == "PLUS") {
-                    result += current_token.value;
-                } else if (op == "MINUS") {
-                    result -= current_token.value;
-                } else {
-                    throw invalid_argument("Numbers cannot be followed by another number");
-                }
-            } else {
-                throw invalid_argument("Input must start with a number and signs cannot be followed by another sign");
-            }
+                if (op == "PLUS") { result += current_token.value; } 
+                else if (op == "MINUS") { result -= current_token.value; }
+            } else { throw invalid_argument("Input must start with a number and signs cannot be followed by another sign"); }
             Token next_token = tokenizer.selectNext();
             op = next_token.type;
             current_token = tokenizer.selectNext();
+            if (op == "INT") { throw invalid_argument("Numbers cannot be followed by another number"); }
+            if (op != "EOF" && current_token.type == "EOF") { throw invalid_argument("Expected number"); }
         }
         return result;
     }
