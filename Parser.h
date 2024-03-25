0dae460bff505792a527d6dd56f13976bb680336
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-03-25 13:18:26 -0300

    Update Parser.h

--- Parser.h
+++ Parser.h
@@ -38,7 +38,11 @@ public:
                 current_token = tokenizer.selectNext();
                 return make_shared<AssignmentNode>(identifier, parse_expression());
             }
-            else { throw invalid_argument("Expected '=' after identifier"); }
+            else { 
+                cout << 1 << endl << 6 << endl << 12 << endl;
+                exit(0);
+                throw invalid_argument("Expected '=' after identifier");
+            }
         }
     }
 
